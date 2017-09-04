49b8d155396d1f0b68dc3d5680195c1483e75a5c
butlerx <butlerx@notthe.cloud>
2017-09-04 21:22:03 +0100

    use async function for cli

--- src/cli.js
+++ src/cli.js
@@ -14,30 +14,31 @@ function config(): Promise {
   });
 }
 
-const optionDefinitions = [
-  {
-    name : 'init',
-    alias: 'i',
-    type : Boolean,
-  },
-];
-
-function init() {
-  const options = commandLineArgs(optionDefinitions);
-  if (options.init) {
-    Promise.all([
-      config(),
-      fs.ensureDir('theme/js'),
-      fs.ensureDir('theme/fonts'),
-      fs.ensureDir('theme/images'),
-      fs.ensureFile('theme/templates/schedule.hbs'),
-      fs.ensureFile('theme/css/main.scss'),
-      fs.ensureFile('source/index.json'),
-    ])
-      .then(console.log('theme created'))
-      .catch(console.error);
-  } else {
-    generate().then(console.log).catch(console.error);
+async function init() {
+  const options = commandLineArgs([
+    {
+      name : 'init',
+      alias: 'i',
+      type : Boolean,
+    },
+  ]);
+  try {
+    if (options.init) {
+      await Promise.all([
+        config(),
+        fs.ensureDir('theme/js'),
+        fs.ensureDir('theme/fonts'),
+        fs.ensureDir('theme/images'),
+        fs.ensureFile('theme/templates/schedule.hbs'),
+        fs.ensureFile('theme/css/main.scss'),
+        fs.ensureFile('source/index.json'),
+      ]);
+      console.log('theme created');
+    } else {
+      console.log(await generate());
+    }
+  } catch (err) {
+    console.error(err);
   }
 }
 
